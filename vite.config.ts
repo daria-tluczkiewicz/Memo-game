9e0f6a2a3dd29501c1b53ca874a3ff941b92b3db
Daria Tłuczkiewicz <daria@Darias-MacBook-Pro.local>
2026-09-25 23:04:22 +0200

    modify vite and vitest config; update tsnofig aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,20 @@
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ mode }) => ({
   base: mode === 'github-pages' ? '/Memo-game/' : '/',
   plugins: [
     react(),
-    svgr()
+    svgr(),
+    tsconfigPaths(),
   ],
   server: {
     host: '0.0.0.0',
     port: 3000,
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(import.meta.dirname, './src'),
-      '@app': path.resolve(import.meta.dirname, './src/app'),
-      '@features': path.resolve(import.meta.dirname, './src/features'),
-      '@components': path.resolve(import.meta.dirname, './src/components'),
-      '@hooks': path.resolve(import.meta.dirname, './src/hooks'),
-      '@styles': path.resolve(import.meta.dirname, './src/styles'),
-      '@types': path.resolve(import.meta.dirname, './src/types'),
-      '@assets': path.resolve(import.meta.dirname, './src/assets'),
-      '@utils': path.resolve(import.meta.dirname, './src/utils'),
-    },
+  test: {
+    environment: 'jsdom',
   },
 }))
